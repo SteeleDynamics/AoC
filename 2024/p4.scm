fa2fb82b3488fbcaa534200a1fb5f5dad1df6037
SteeleDynamics <SteeleDynamics@gmail.com>
2024-12-13 14:06:47 -0500

    Completed p3 and p4

--- 2024/p4.scm
+++ 2024/p4.scm
@@ -58,7 +58,35 @@
   (let ((diff (adj-diff x)))
     (and (strictly-monotonic? diff) (less-than-four? diff))))
 
-;; solving p3
-(define p3-input (read-input "p3-input.txt"))
-(define p3-ans (length (filter safe? p3-input)))
-p3-ans
+;; take procedure
+(define (take x n)
+  (cond ((< n 0) (error 'take "Index less than zero" n))
+        ((or (null? x) (zero? n)) '())
+        (else (cons (car x) (take (cdr x) (- n 1))))))
+
+;; drop procedure
+(define (drop x n)
+  (cond ((< n 0) (error 'drop "Index less than zero" n))
+        ((or (null? x) (zero? n)) x)
+        (else (drop (cdr x) (- n 1)))))
+
+;; del-ref procedure
+(define (del-ref x n) (append (take x n) (drop x (+ n 1))))
+
+;; safe-ish-iter procedure
+(define (safe-ish-iter x k n)
+  (cond ((>= k n) #f)
+        ((safe? (del-ref x k)) #t)
+        (else (safe-ish-iter x (+ k 1) n))))
+
+;; safe-ish? predicate procedure
+(define (safe-ish? x)
+  (safe-ish-iter x 0 (length x)))
+
+;; solving p4
+(define p4-input (read-input "p4-input.txt"))
+(define safe (filter safe? p4-input))
+(define unsafe (filter (lambda (x) (not (safe? x))) p4-input))
+(define safe-ish (filter safe-ish? unsafe))
+(define p4-ans (+ (length safe) (length safe-ish)))
+p4-ans
